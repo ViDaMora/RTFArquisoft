bbf89aaa832af1f9644cbe7f56612692359b1df2
Belthgorr <vdaniel.mora@udea.edu.co>
2020-05-24 23:33:05 -0500

    form

---
+++ public/main.js
@@ -0,0 +1,96 @@
+let documento= document.querySelector('.input-documento');
+let doc= document.querySelector('.input-documento-delete');
+let nombre= document.querySelector('.input-nombre');
+let apellidos= document.querySelector('.input-apellido');
+let email= document.querySelector('.input-email');
+let tipotarjeta= document.querySelector('.input-tipo');
+let numerotarjeta= document.querySelector('.input-numero');
+let cargo= document.querySelector('.input-cargo');
+let valorFacturas= document.querySelector('.input-facturas');
+let divMensaje= document.querySelector('.mensaje');
+
+
+
+$("#update-button").click(function (e) { 
+       
+    $('#EditModal').modal('toggle')
+    
+});
+
+
+$("#delete-button").click(function (e) { 
+       
+  $('#DeleteModal').modal('toggle')
+  
+});
+
+$(".btn-editu").click(function (e) { 
+       
+    //alert(`User info: ${email.value} ${documento.value} ${nombre.value} ${apellidos.value} ${tipotarjeta.value} ${numerotarjeta.value} ${cargo.value} ${valorFacturas.value}`);
+    fetch('/usuarios', {
+        method: 'put',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            nombre: `${nombre.value}`,
+            apellidos: `${apellidos.value}`,
+            email: `${email.value}`,
+            documento: `${documento.value}`,
+            tipotarjeta: `${tipotarjeta.value}`,
+            numerotarjeta: `${numerotarjeta.value}`,
+            cargo: `${cargo.value}`,
+            valorFacturas:`${valorFacturas.value}`,
+
+          })
+      })
+      .then(res => {
+        if (res.ok) return res.json()
+      })
+      .then(response => {
+        window.location.reload(true)
+      })
+      $('#EditModal').modal('hide');
+});
+
+$(".btn-deleteu").click(function (e) { 
+  if(doc.value===""){
+   alert("Ingrese un documento");
+   return;
+  }
+    
+  alert(`User info: ${doc.value}`);
+  fetch('/usuarios', {
+      method: 'delete',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+          documento: `${doc.value}`,
+          
+        })
+    })
+    .then(res => {
+      if (res.ok) return res.json()
+    })
+    .then(response => {
+      if(response === 'No hay usuarios para borrar'){
+        divMensaje.textContent='No existe usuario con ese ID, intente de nuevo'
+      }else{
+       
+      window.location.reload(true)
+      }
+
+    })
+   $('#DeleteModal').modal('hide'); 
+   divMensaje.textContent=''
+});
+
+/*update.addEventListener('click', ()=> {
+    fetch('/usuarios', {
+        method: 'put',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            nombre: 'Dieguito',
+            apellidos: 'Maradona',
+            email: 'telojuro@gmail.com',
+            documento: '100000'
+          })
+      })
+}); */
---
+++ server.js
@@ -0,0 +1,101 @@
+
+const express = require("express");
+const app = express();
+const body_parser = require("body-parser");
+
+// parse JSON (application/json content-type)
+app.use(body_parser.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.use(body_parser.json());    
+app.set('view engine', 'ejs')
+const port = 4000;
+
+// << db setup >>
+const db = require("./db");
+const dbName = "dbtest";
+const collectionName = "test";
+
+/// << db init >>
+db.initialize(dbName, collectionName, function(dbCollection) { // successCallback
+    // get all items
+  
+
+    // << db CRUD routes >>
+    
+app.get('/', (req,res)=>{
+
+    res.render('index.ejs');
+      
+});
+app.get('/usuarios', (req,res)=>{
+    dbCollection.find().toArray()
+    .then(results => {
+      res.render('usuarios.ejs',{results})
+      })
+      .catch(err => console.error(err));
+    
+   
+    
+});
+app.get('/registro', (req,res)=>{
+
+    res.render('registro.ejs');
+      
+});
+app.put('/usuarios', (req, res) => {
+
+
+    dbCollection.findOneAndUpdate(
+
+        { documento: req.body.documento},
+        {
+        $set: {
+            nombre: req.body.nombre,
+            apellidos: req.body.apellidos,
+            email: req.body.email,
+            tipotarjeta: req.body.tipotarjeta,
+            numerotarjeta: req.body.numerotarjeta,
+            cargo: req.body.cargo,
+            valorFacturas: req.body.valorFacturas,
+        }
+         },
+       {
+           upsert:true
+       }
+
+
+      )
+        .then(result => {
+            res.json('Success')
+        })
+        .catch(error => console.error(error))
+  });
+  app.delete('/usuarios', (req, res) => {
+    dbCollection.deleteOne(
+      { documento: req.body.documento }
+    )
+      .then(result => {
+          if(result.deletedCount === 0){
+            return res.json('No hay usuarios para borrar')
+          }
+        res.json(`Deleted documento ${req.body.documento} `)
+      })
+      .catch(error => console.error(error))
+  });
+
+app.post('/registro', (req, res) => {
+    dbCollection.insertOne(req.body)
+    .then(result => {
+       res.redirect('/registro')
+    })
+        .catch(err => console.error(err));
+  })
+  
+  
+}, function(err) { // failureCallback
+    throw (err);
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at ${port}`);
+});
